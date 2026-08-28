fa36aa636adb71701a65abec54049a430075fb89
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-08-28 15:51:45 +0200

    Lektion 1

---
+++ lecture-1/Dialog/Dialog.App/Program.cs
@@ -0,0 +1,25 @@
+﻿List<string> names = [];
+
+while (true)
+{
+    Console.Write("Ange namn: ");
+    string? nameInput = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(nameInput))
+        names.Add(nameInput.Trim());
+
+    Console.Write("Ange en till medlem? (y/n): ");
+    string? option = Console.ReadLine();
+
+    Console.Clear();
+
+    if (option?.Trim().Equals("n", StringComparison.OrdinalIgnoreCase) == true)
+        break;
+}
+
+Console.WriteLine("Gruppens medlemmar:");
+
+foreach (string name in names)
+    Console.WriteLine($"- {name}");
+
+
