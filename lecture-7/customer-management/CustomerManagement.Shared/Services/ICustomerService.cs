8dd6aebe363154d47df3e385341e294f9773fd4b
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-09-16 10:06:15 +0200

    Lektion 7

---
+++ lecture-7/customer-management/CustomerManagement.Shared/Services/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using CustomerManagement.Shared.Models;
+
+namespace CustomerManagement.Shared.Services;
+
+public interface ICustomerService
+{
+    Customer Create(string name, string email);
+    bool Delete(Guid customerId);
+    IReadOnlyList<Customer> GetAll();
+
+}
---
+++ lecture-7/customer-management/CustomerManagement.Shared/Services/InMemoryCustomerService.cs
@@ -0,0 +1,46 @@
+﻿using CustomerManagement.Shared.Models;
+using System.Net.Mail;
+
+namespace CustomerManagement.Shared.Services;
+
+public class InMemoryCustomerService : ICustomerService
+{
+    private readonly List<Customer> _customers = [];
+
+    public Customer Create(string name, string email)
+    {
+        if (string.IsNullOrWhiteSpace(name)) 
+            throw new ArgumentException("Enter a customer name.");
+
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Enter a customer email address.");
+
+        name = name.Trim();
+        email = email.Trim().ToLower();
+
+        if (!MailAddress.TryCreate(email, out var emailAddress) || emailAddress.Address != email)
+            throw new ArgumentException("Enter a valid email address.");
+
+        var customer = new Customer
+        {
+            Name = name,
+            Email = emailAddress.Address
+        };
+
+        _customers.Add(customer);
+
+        return customer;
+    }
+
+    public bool Delete(Guid customerId)
+    {
+        var customer = _customers.FirstOrDefault(customer => customer.Id == customerId);
+
+        if (customer is null)
+            return false;
+
+        return _customers.Remove(customer);
+    }
+
+    public IReadOnlyCollection<Customer> GetAll() => _customers;
+}
