ad1b8161a5212bd29277961d9b53b16ecfba139b
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-09-16 14:41:38 +0200

    Lektion 7

--- lecture-7/customer-management/CustomerManagement.Wpf/MainWindow.xaml.cs
+++ lecture-7/customer-management/CustomerManagement.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CustomerManagement.Shared.Services;
+﻿using CustomerManagement.Shared.Models;
+using CustomerManagement.Shared.Services;
 using System.Windows;
 
 namespace CustomerManagement.Wpf;
@@ -12,5 +13,51 @@ public partial class MainWindow : Window
         _customerService = customerSerivce;
 
         InitializeComponent();
+
+        LoadCustomers();
+    }
+
+    private void LoadCustomers()
+    {
+        var customers = _customerService.GetAll();
+
+        CustomersDataGrid.ItemsSource = customers;
+        StatusTextBlock.Text = $"Customers: {customers.Count}";
+    }
+
+    private void AddCustomerButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            _customerService.Create(NameTextBox.Text, EmailTextBox.Text);
+
+            LoadCustomers();
+
+            NameTextBox.Clear();
+            EmailTextBox.Clear();
+            NameTextBox.Focus();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(this, ex.Message, "Validation", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+
+    private void DeleteSelectedButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (CustomersDataGrid.SelectedItem is not Customer customer)
+        {
+            MessageBox.Show(this, "Select a cstuomer to delete", "No Customer Selected", MessageBoxButton.OK, MessageBoxImage.Information);
+            return;
+        }
+
+        var confirmation = MessageBox.Show(this, $"Delete {customer.Name}", "Confirm Deletion", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+        if (confirmation != MessageBoxResult.Yes)
+            return;
+
+        var deleted = _customerService.Delete(customer.Id);
+
+        LoadCustomers();
     }
 }
