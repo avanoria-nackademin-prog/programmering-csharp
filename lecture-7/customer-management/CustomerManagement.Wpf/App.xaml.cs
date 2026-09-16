493539a6d63027609ae08723e3a4932079baf201
Hans Mattin-Lassei <hans.mattin-lassei@avanoria.se>
2026-09-16 14:12:35 +0200

    Lektion 7

---
+++ lecture-7/customer-management/CustomerManagement.Wpf/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using CustomerManagement.Shared.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace CustomerManagement.Wpf;
+
+public partial class App : Application
+{
+    private ServiceProvider? _serviceProvider;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var services = new ServiceCollection();
+
+        services.AddSingleton<ICustomerService, InMemoryCustomerService>();
+        services.AddTransient<MainWindow>();
+
+        _serviceProvider = services.BuildServiceProvider();
+
+        MainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+    }
+
+}
---
+++ lecture-7/customer-management/CustomerManagement.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CustomerManagement.Shared.Services;
+using System.Windows;
+
+namespace CustomerManagement.Wpf;
+
+public partial class MainWindow : Window
+{
+    private readonly ICustomerService _customerService;
+
+    public MainWindow(ICustomerService customerSerivce)
+    {
+        _customerService = customerSerivce;
+
+        InitializeComponent();
+    }
+}
